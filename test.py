31adf6c3cc68bd86271288fac5d05e6045a734df
Szymon Wąchała <96399051+Szymon423@users.noreply.github.com>
2022-07-18 15:07:57 +0200

    debugging

---
+++ test.py
@@ -0,0 +1,43 @@
+from machine import Pin
+from time import sleep_ms
+
+## definition of IOpins
+# status LED
+greenLED_pin = Pin(8, Pin.OUT)
+yellowLED_pin = Pin(9, Pin.OUT)
+redLED_pin = Pin(10, Pin.OUT)
+
+# encoder pins
+encA_pin = Pin(11, Pin.IN, Pin.PULL_UP)
+encB_pin = Pin(12, Pin.IN, Pin.PULL_UP)
+
+# relays pins
+relG_pin = Pin(3, Pin.OUT)
+relR_pin = Pin(4, Pin.OUT)
+
+## setting initial values of pins
+# status LED
+greenLED_pin.value(0)
+yellowLED_pin.value(0)
+redLED_pin.value(0)
+
+# relays pins
+relG_pin.value(0)
+relR_pin.value(0)
+
+
+# MAIN loop
+while True:
+    greenLED_pin.toggle()
+    sleep_ms(100)
+    greenLED_pin.toggle()
+    sleep_ms(100)
+    yellowLED_pin.toggle()
+    sleep_ms(100)
+    yellowLED_pin.toggle()
+    sleep_ms(100)
+    redLED_pin.toggle()
+    sleep_ms(100)
+    redLED_pin.toggle()
+    sleep_ms(100)
+

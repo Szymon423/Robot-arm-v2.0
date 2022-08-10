6d5838953e9595008353531e9fb840e6ae5ddcfa
Szymon Wąchała <96399051+Szymon423@users.noreply.github.com>
2022-08-10 15:08:39 +0200

    quite smooth

--- master.py
+++ master.py
@@ -3,35 +3,56 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 
-serialPort = serial.Serial(port="COM8", baudrate=115200,
+serialPort = serial.Serial(port="COM7", baudrate=115200,
                            bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
 serialString = ""
 i = 0
 
-arr1 = np.linspace(0, 2000, 2000, dtype=int)
-arr2 = np.linspace(2000, 0, 2000, dtype=int)
 
-arr_pos = np.concatenate((arr1, arr2), axis=0, out=None)
+steps = 1000
+arr1 = np.linspace(0, 2000, steps+1, dtype=int)
+arr2 = np.linspace(2000, 0, steps+1, dtype=int)
+x = np.linspace(0, 4000, steps)
+y = 4000 - 4000 * np.cos(x / (2 * np.pi * 50))
 
-plt.plot(arr_pos)
-plt.show()
 
-time_max = 4000     # ms
+# arr_pos = np.concatenate((arr1, arr2), axis=0, out=None)
 
-time_div = arr_pos.size / time_max
-print(time_div)
+arr_pos = np.asarray(y, dtype=int)
 
-arr_speed = [(np.abs(arr_pos[i] - arr_pos[i-1]) / time_div * 1000.0) for i in range(1, time_max)]
+# print(arr_pos)
+time_max = 10000     # ms
 
+time_div = time_max / (arr_pos.size - 2) # 40000 / 40 = 1000
+print("time_div: " + str(time_div))
+print("różnica: " + str(np.abs(arr_pos[2] - arr_pos[1])))
+
+arr_speed =[0.625 * (np.abs(arr_pos[i+1] - arr_pos[i]) / time_div * 1000.0) for i in range(0, arr_pos.size-1)]
+arr_speed.insert(0, arr_speed[0])
+arr_speed = np.asarray(arr_speed)
+
+arr_speed_int = np.array(arr_speed, dtype=int)
+
+# print(arr_speed_float)
+print("speed is set to: " + str(arr_speed_int[1]))
 cnt = 0
+dir = 1
+first = True
 
 while (True):
 
-    string_ = str(arr_pos[cnt]) + "x" + str(arr_speed[cnt]) + "n"
-    serialPort.write(str.encode(string_))
-    cnt += 1
+    if cnt >= arr_pos.size or (cnt == 0 and not first):
+        dir *= -1
+        first = False
     
-    time.sleep(time_div / 1000.0)
 
-    print(serialPort.readline())
+    start = int(round(time.time() * 1000))
+    string_ = str(arr_pos[cnt]) + "x" + str(arr_speed_int[cnt]) + "n"
+    serialPort.write(str.encode(string_))
+    
+    time.sleep(time_div / 1000.0)   # 1000 / 1000 = 1s
+    
+    stop = int(round(time.time() * 1000))
+    print("pos: " + str(arr_pos[cnt]) + " spe: " + str(arr_speed_int[cnt]) + " time: " + str(stop-start))
+    cnt += dir
